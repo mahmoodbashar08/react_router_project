558d644b4737f1ced247cc7f846f60374621fccb
mahmood <79115153+mahmoodbashar08@users.noreply.github.com>
2022-06-21 02:34:11 +0300

    .

--- react_router_project/src/App.js
+++ react_router_project/src/App.js
@@ -1,3 +1,14 @@
+import Hrader from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import About from "./components/About";
+import Missing from "./components/Missing";
+import { Route, Switch, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 function App() {
   return <div className="App"></div>;
 }
